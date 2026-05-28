dc1121fd33bf7c44979901eec73fa975075bbaed
ayushah08 <ayushah810@gmail.com>
2026-05-28 11:46:19 +0530

    fixed cors issue

--- backend/src/main/java/com/coachassist/backend/security/config/CorsConfig.java
+++ backend/src/main/java/com/coachassist/backend/security/config/CorsConfig.java
@@ -2,11 +2,8 @@ package com.coachassist.backend.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.cors.CorsConfiguration;
-
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
@@ -17,32 +14,32 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
 
-        CorsConfiguration config =
-                new CorsConfiguration();
+        CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
 
-        config.setAllowedOrigins(
-                List.of(
-                        "http://localhost:5173"
-                )
-        );
+        config.setAllowedOriginPatterns(List.of(
+                "http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "http://127.0.0.1:5173",
+                "http://localhost:5173",
+                "*"
+        ));
 
-        config.setAllowedHeaders(
-                List.of("*")
-        );
+        config.setAllowedHeaders(List.of("*"));
 
-        config.setAllowedMethods(
-                List.of("*")
-        );
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration(
-                "/**",
-                config
-        );
+        source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
     }
